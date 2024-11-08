4c6fb2a15f0e0a46b3048847080e78d87c1d2b8d
Ajaykadu <kaduajay1122@gmail.com>
2024-11-08 23:05:10 +0530

    Fragment with viewPager2

---
+++ Day5/App3/app/src/main/java/com/example/app3/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.app3;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewPager2 viewPager2;
+
+    ViewAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager2 = findViewById(R.id.viewPager2);
+
+        adapter = new ViewAdapter(this);
+        viewPager2.setAdapter(adapter);
+
+    }
+}
--- Day5/App3/app/src/main/java/com/example/app3/ViewAdapter.java
+++ Day5/App3/app/src/main/java/com/example/app3/ViewAdapter.java
@@ -1,4 +1,34 @@
 package com.example.app3;
 
-public class ViewAdapter {
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class ViewAdapter extends FragmentStateAdapter {
+    public ViewAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position)
+        {
+            case 0:
+                return new FirstFragment();
+            case 1:
+                return new SecondFragment();
+            case 2:
+                return new ThirdFragment();
+            case 3:
+                return new ForthFragment();
+        }
+        return null;
+    }
+
+    @Override
+    public int getItemCount() {
+        return 4;
+    }
 }
